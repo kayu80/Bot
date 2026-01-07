afdf1ac3f1749ad01ef8155d22e1804302d983d0
kayu80 <Kayu80@mail.ru>
2026-01-07 22:26:24 +0300

    Update TextUtil.java

--- p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/utils/TextUtil.java
+++ p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/utils/TextUtil.java
@@ -2,7 +2,18 @@ package com.skillbox.cryptobot.utils;
 
 public class TextUtil {
 
-    public static String toString(double value) {
-        return String.format("%.3f", value);
+    public static String formatDecimal(double number) {
+        return String.format("%.2f", number);
+    }
+
+    public static String formatLargeNumber(long number) {
+        return String.format("%,d", number);
+    }
+
+    public static String truncateText(String text, int maxLength) {
+        if (text.length() <= maxLength) {
+            return text;
+        }
+        return text.substring(0, maxLength - 3) + "...";
     }
 }
