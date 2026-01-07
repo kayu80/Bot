48cbb2272fe3bbef162f9bd47cf77facc7ba92b0
kayu80 <Kayu80@mail.ru>
2026-01-07 21:48:39 +0300

    Update StartCommand.java

--- p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/bot/command/StartCommand.java
+++ p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/bot/command/StartCommand.java
@@ -1,22 +1,22 @@
 package com.skillbox.cryptobot.bot.command;
 
+import com.skillbox.cryptobot.model.Subscriber;
+import com.skillbox.cryptobot.repository.SubscriberRepository;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
-/**
- * Обработка команды начала работы с ботом
- */
 @Service
-@AllArgsConstructor
 @Slf4j
+@AllArgsConstructor
 public class StartCommand implements IBotCommand {
+    private final SubscriberRepository subscriberRepo;
 
     @Override
     public String getCommandIdentifier() {
@@ -25,23 +25,23 @@ public class StartCommand implements IBotCommand {
 
     @Override
     public String getDescription() {
-        return "Запускает бота";
+        return "Начало работы с ботом.";
     }
 
+    @SneakyThrows
     @Override
     public void processMessage(AbsSender absSender, Message message, String[] arguments) {
+        long telegramId = message.getFrom().getId();
+
+        if (!subscriberRepo.existsByTelegramId(telegramId)) {
+            double targetPrice = 0;
+            subscriberRepo.save(new Subscriber(telegramId));
+        }
+
         SendMessage answer = new SendMessage();
         answer.setChatId(message.getChatId());
+        answer.setText("Добро пожаловать в крипто-бот! Доступные команды:\n/get_price - Получить текущую цену биткоина\n/subscribe - Подписаться на уведомления о цене\n/unsubscribe - Отменить подписку");
 
-        answer.setText("""
-                Привет! Данный бот помогает отслеживать стоимость биткоина.
-                Поддерживаемые команды:
-                 /get_price - получить стоимость биткоина
-                """);
-        try {
-            absSender.execute(answer);
-        } catch (TelegramApiException e) {
-            log.error("Error occurred in /start command", e);
-        }
+        absSender.execute(answer);
     }
 }
