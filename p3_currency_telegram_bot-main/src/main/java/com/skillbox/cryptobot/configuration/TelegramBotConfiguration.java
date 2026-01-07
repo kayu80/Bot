6eed059a24e450ef6629d27c7c2285b08d3ec273
kayu80 <Kayu80@mail.ru>
2026-01-07 21:55:11 +0300

    Update TelegramBotConfiguration.java

--- p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/configuration/TelegramBotConfiguration.java
+++ p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/configuration/TelegramBotConfiguration.java
@@ -1,25 +1,25 @@
-package com.skillbox.cryptobot.configuration;
+package com.skillbox.cryptobot.bot.config;
 
-
-import com.skillbox.cryptobot.bot.CryptoBot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import com.skillbox.cryptobot.bot.CryptoBot;
 
 @Configuration
 @Slf4j
 public class TelegramBotConfiguration {
+
     @Bean
-    TelegramBotsApi telegramBotsApi(CryptoBot cryptoBot) {
+    public TelegramBotsApi telegramBotsApi(CryptoBot cryptoBot) {
         TelegramBotsApi botsApi = null;
         try {
             botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(cryptoBot);
         } catch (TelegramApiException e) {
-            log.error("Error occurred while sending message to telegram!", e);
+            log.error("Ошибка регистрации бота в Telegram!", e);
         }
         return botsApi;
     }
