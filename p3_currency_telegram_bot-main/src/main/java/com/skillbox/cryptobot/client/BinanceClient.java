4d60a85c6cab0c6006ea12ec177f20367167a2cc
kayu80 <Kayu80@mail.ru>
2026-01-07 22:10:11 +0300

    Update BinanceClient.java

--- p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/client/BinanceClient.java
+++ p3_currency_telegram_bot-main/src/main/java/com/skillbox/cryptobot/client/BinanceClient.java
@@ -1,11 +1,6 @@
 package com.skillbox.cryptobot.client;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
@@ -13,10 +8,14 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 @Service
 @Slf4j
 public class BinanceClient {
+
     private final HttpGet httpGet;
     private final ObjectMapper mapper;
     private final HttpClient httpClient;
@@ -28,13 +27,14 @@ public class BinanceClient {
                 .setSSLHostnameVerifier(new NoopHostnameVerifier())
                 .build();
     }
+
     public double getBitcoinPrice() throws IOException {
-        log.info("Performing client call to binanceApi to get bitcoin price");
+        log.info("Выполнение запроса к Binance API для получения цены биткоина.");
         try {
             return mapper.readTree(EntityUtils.toString(httpClient.execute(httpGet).getEntity()))
                     .path("price").asDouble();
         } catch (IOException e) {
-            log.error("Error while getting price from binance", e);
+            log.error("Ошибка при получении цены биткоина от Binance.", e);
             throw e;
         }
     }
